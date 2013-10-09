a899c24f5af7592e79f02fc1db9a749ec8d22736
Zoltan Ormandi <zoltan.ormandi@emarsys.com>
2013-10-09 08:12:46 +0200

    refactored DataImporter

--- lib/cranium/data_importer.rb
+++ lib/cranium/data_importer.rb
@@ -1,19 +1,22 @@
 class Cranium::DataImporter
 
   def import(import_definition)
-
     Cranium::Database.connection.transaction do
-      if import_definition.merge_fields.empty?
-        importer = Cranium::ImportStrategy::Delta.new(import_definition)
-      else
-        importer = Cranium::ImportStrategy::Merge.new(import_definition)
-      end
-
-      importer.import
-
+      importer_for_definition(import_definition).import
       Cranium.application.apply_hook(:after_import)
     end
+  end
 
+
+
+  private
+
+  def importer_for_definition(import_definition)
+    if import_definition.merge_fields.empty?
+      Cranium::ImportStrategy::Delta.new(import_definition)
+    else
+      Cranium::ImportStrategy::Merge.new(import_definition)
+    end
   end
 
 end
