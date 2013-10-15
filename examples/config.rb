f75563cfd2f3e45df4931d49b71dc442ae358624
Zoltan Ormandi <zoltan.ormandi@emarsys.com>
2013-10-15 12:46:17 +0200

    example files added

---
+++ examples/config.rb
@@ -0,0 +1,10 @@
+require 'logger'
+
+Cranium.configure do |config|
+  config.greenplum_connection_string = "postgres://cranium:cranium@192.168.56.42:5432/cranium"
+  config.gpfdist_url = "gpfdhost:8123"
+  config.gpfdist_home_directory = ENV["GPFDIST_HOME"]
+  config.upload_directory = "cranium_build"
+  config.archive_directory = "cranium_archive"
+  config.loggers << Logger.new("log/application.log")
+end
