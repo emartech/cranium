4c3280f6634d1d4b4b179be402764de7ec11b5a9
Zoltan Ormandi <zoltan.ormandi@emarsys.com>
2013-11-04 11:58:57 +0100

    deduplication example

---
+++ examples/deduplication.rb
@@ -0,0 +1,26 @@
+require_relative 'config'
+
+source :sales_items do
+  file "sales_items*.csv"
+  field :order_id, String
+  field :date, Date
+  field :customer, Integer
+  field :item, String
+  field :item_name, String
+  field :quantity, Float
+  field :c_sales_amount, Float
+end
+
+source :products do
+  field :item_id
+  field :item_name
+end
+
+
+deduplicate :sales_items => :products, by: [:item]
+
+# Equivalent to
+#
+#transform :sales_items => :products do |record|
+#  deduplicate by: [:item]
+#end
