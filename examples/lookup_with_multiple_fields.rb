4c3d84d2eda279f4473abf6d101de6eba979a44e
Balazs Szerencsi <balazs.szerencsi@emarsys.com>
2013-11-08 16:06:14 +0100

    example

---
+++ examples/lookup_with_multiple_fields.rb
@@ -0,0 +1,25 @@
+require_relative 'config'
+
+source :purchases do
+  field :user_id, String
+  field :amount, String
+end
+
+source :transformed_purchases do
+  field :contact_key, Integer
+  field :amount, String
+end
+
+transform :purchases => :transformed_purchases do |record|
+  record[:contact_key] = lookup :contact_key,
+                                from_table: :dim_contact,
+                                match: {:user_id => record[:user_id], :another_field => record[:another_value]},
+                                if_not_found_then_insert: { contact_key: next_value_in_sequence("dim_contact_contact_key_seq"),
+                                                            name: "Unknown contact #{record[:user_id]}" }
+end
+
+import :transformed_purchases do
+  into :fct_purchases
+  put :contact_key
+  put :amount
+end
