3477bd6c82dbd35509c4f42cd5d50e307bcf5e5a
Rafael Ordog <rafael.oerdoeg@emarsys.com>
2013-11-12 10:01:07 +0100

    Refactored lookup function

--- lib/cranium/transformation/index.rb
+++ lib/cranium/transformation/index.rb
@@ -9,22 +9,14 @@ class Cranium::Transformation::Index
   def lookup(field_name, options)
     validate options
 
-    if options.has_key? :match
-      keys = options[:match].values
-      key_fields = options[:match].keys
-    else
-      keys = [options[:to_value]]
-      key_fields = [options[:match_column]]
-    end
+    cache = cache_for(options[:from_table], key_fields(options), field_name)
 
-    cache = cache_for(options[:from_table], key_fields, field_name)
-
-    if cache.has_key? keys
-      cache[keys]
+    if cache.has_key? keys(options)
+      cache[keys(options)]
     elsif options.has_key? :if_not_found_then
       options[:if_not_found_then]
     elsif options.has_key? :if_not_found_then_insert
-      cache[keys] = Cranium::DimensionManager.for(options[:from_table], field_name).insert(default_value_record(options))
+      cache[keys(options)] = Cranium::DimensionManager.for(options[:from_table], field_name).insert(default_value_record(options))
     else
       :not_found
     end
@@ -33,8 +25,7 @@ class Cranium::Transformation::Index
 
 
   def validate(options)
-    raise ArgumentError, "Cannot specify both :if_not_found_then and :if_not_found_then_insert options" if
-      options.has_key? :if_not_found_then_insert and options.has_key? :if_not_found_then
+    raise ArgumentError, "Cannot specify both :if_not_found_then and :if_not_found_then_insert options" if options.has_key? :if_not_found_then_insert and options.has_key? :if_not_found_then
   end
 
 
@@ -55,4 +46,27 @@ class Cranium::Transformation::Index
   def cache_for(table_name, key_fields, value_field)
     @indexes[[table_name, key_fields, value_field]] ||= Cranium::DimensionManager.for(table_name, key_fields).create_cache_for_field(value_field)
   end
+
+
+
+  def key_fields(options)
+    if options.has_key? :match
+      key_fields_tmp = options[:match].keys
+    else
+      key_fields_tmp = [options[:match_column]]
+    end
+    key_fields_tmp
+  end
+
+
+
+  def keys(options)
+    if options.has_key? :match
+      keys_tmp = options[:match].values
+    else
+      keys_tmp = [options[:to_value]]
+    end
+    keys_tmp
+  end
+
 end
