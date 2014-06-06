ba497c2788be76cea1c04a7af740bbee1a410163
Istvan Demeter <istvan.demeter@emarsys.com>
2014-06-06 15:32:27 +0200

    Replace hostnames in test configs with the Vagrant VM's IP

--- examples/config.rb
+++ examples/config.rb
@@ -2,7 +2,7 @@ require 'logger'
 
 Cranium.configure do |config|
   config.greenplum_connection_string = "postgres://cranium:cranium@192.168.56.42:5432/cranium"
-  config.gpfdist_url = "gpfdhost:8123"
+  config.gpfdist_url = "192.168.56.42:8123"
   config.gpfdist_home_directory = ENV["GPFDIST_HOME"]
   config.upload_directory = "cranium_build"
   config.archive_directory = "cranium_archive"
