de709e57f7a1ab6a6804a39dbc0976d1e76daa65
thilonel <nagy.daniel@emarsys.com>
2016-06-06 15:09:59 +0200

    test: file utils handle () characters in path

--- spec/cranium/file_utils_spec.rb
+++ spec/cranium/file_utils_spec.rb
@@ -3,19 +3,30 @@ require_relative '../spec_helper'
 describe Cranium::FileUtils do
 
   describe '#line_count' do
-    let(:path) { '/tmp/3_lines.txt' }
     let(:number_of_lines) { 3 }
 
-    before do
-      File.open(path, 'w') do |file|
-        number_of_lines.times { |i| file.puts i }
-      end
+    it 'returns the number of lines the file has' do
+      path = '/tmp/3_lines.txt'
+      create_test_file path, number_of_lines
+
+      expect(described_class.line_count(path)).to eq(number_of_lines)
+      File.delete path
     end
 
-    after { File.delete path }
+    it 'handles () characters in path' do
+      path = '/tmp/such_copy_of_a_copy(2).txt'
+      create_test_file path, number_of_lines
 
-    it 'returns the number of lines the file has' do
-      expect(described_class.line_count(path)).to eq(number_of_lines)
+      expect { described_class.line_count(path) }.not_to raise_error
+      File.delete path
+    end
+  end
+
+
+
+  def create_test_file(path, number_of_lines)
+    File.open(path, 'w') do |file|
+      number_of_lines.times { |i| file.puts i }
     end
   end
 
